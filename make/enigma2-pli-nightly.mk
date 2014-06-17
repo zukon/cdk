28d24224c61bafce19c031dffac6b54f05661a5f
max10 <max_10@gmx.de>
2014-06-17 13:10:30 +0200

    rename old enigma2-pli-nightly diff

--- make/enigma2-pli-nightly.mk
+++ make/enigma2-pli-nightly.mk
@@ -38,29 +38,21 @@ $(D)/enigma2-pli-nightly.do_prepare: | $(ENIGMA2_DEPS)
 	echo " 0) Newest                 - E2 OpenPli gstreamer / libplayer3    (Can fail due to outdated patch)     "; \
 	echo "========================================================================================================"; \
 	echo " 1) use your own e2 git dir without patchfile"; \
-	echo " 2) inactive"; \
 	echo "========================================================================================================"; \
-	echo " 3) Mon, 28 Jan 2013 21:30 - E2 OpenPli gstreamer / libplayer3 ce3b90e73e88660bafe900f781d434dd6bd25f71"; \
-	echo " 4) Sat,  2 Mar 2013 21:36 - E2 OpenPli gstreamer / libplayer3 4361a969cde00cd37d6d17933f2621ea49b5a30a"; \
-	echo " 5) Fri, 10 May 2013 15:43 - E2 OpenPli gstreamer / libplayer3 20a5a1f00cdadeff6d0e02861cf7ba8436fc49c5"; \
-	echo " 6) Mon, 28 Oct 2013 14:16 - E2 OpenPli gstreamer / libplayer3 16cada2e02407c588382b95acc8f016ec4a29452"; \
-	echo " 7) Mon, 30 Dec 2013 18:33 - E2 OpenPli gstreamer / libplayer3 715a3024ad7ae3e89dad039bfb8ae49350552c39"; \
-	echo " 8) Sun, 23 Feb 2014 10:05 - E2 OpenPli gstreamer / libplayer3 e858a47a49c4fd8cdf22b29ea7278e6b4a2bddae"; \
-	echo " 9) Tue, 25 Mar 2014 18:17 - E2 OpenPli gstreamer / libplayer3 7272840d7db98a88f5c8b2882cc78d7ddc04e5e6"; \
+	echo " 2) Tue, 17 Jun 2014 07:44 - E2 OpenPli gstreamer / libplayer3 b670ebecf90dc4651b2862ebf448bca370d69fef";
+	echo " 3) Mon, 30 Dec 2013 18:33 - E2 OpenPli gstreamer / libplayer3 715a3024ad7ae3e89dad039bfb8ae49350552c39"; \
+	echo " 4) Sun, 23 Feb 2014 10:05 - E2 OpenPli gstreamer / libplayer3 e858a47a49c4fd8cdf22b29ea7278e6b4a2bddae"; \
+	echo " 5) Tue, 25 Mar 2014 18:17 - E2 OpenPli gstreamer / libplayer3 7272840d7db98a88f5c8b2882cc78d7ddc04e5e6"; \
 	echo "========================================================================================================"; \
 	echo "Media Framwork : $(MEDIAFW)"; \
 	echo "External LCD   : $(EXTERNALLCD)"; \
 	read -p "Select         : "; \
 	[ "$$REPLY" == "0" ] && DIFF="0"; \
 	[ "$$REPLY" == "1" ] && DIFF="1" && REVISION=""; \
-	[ "$$REPLY" == "2" ] && DIFF="2" && REVISION=""; \
-	[ "$$REPLY" == "3" ] && DIFF="3" && REVISION="ce3b90e73e88660bafe900f781d434dd6bd25f71"; \
-	[ "$$REPLY" == "4" ] && DIFF="4" && REVISION="4361a969cde00cd37d6d17933f2621ea49b5a30a"; \
-	[ "$$REPLY" == "5" ] && DIFF="5" && REVISION="20a5a1f00cdadeff6d0e02861cf7ba8436fc49c5"; \
-	[ "$$REPLY" == "6" ] && DIFF="6" && REVISION="16cada2e02407c588382b95acc8f016ec4a29452"; \
-	[ "$$REPLY" == "7" ] && DIFF="7" && REVISION="715a3024ad7ae3e89dad039bfb8ae49350552c39"; \
-	[ "$$REPLY" == "8" ] && DIFF="8" && REVISION="e858a47a49c4fd8cdf22b29ea7278e6b4a2bddae"; \
-	[ "$$REPLY" == "9" ] && DIFF="9" && REVISION="7272840d7db98a88f5c8b2882cc78d7ddc04e5e6"; \
+	[ "$$REPLY" == "2" ] && DIFF="2" && REVISION="b670ebecf90dc4651b2862ebf448bca370d69fef"; \
+	[ "$$REPLY" == "7" ] && DIFF="3" && REVISION="715a3024ad7ae3e89dad039bfb8ae49350552c39"; \
+	[ "$$REPLY" == "8" ] && DIFF="4" && REVISION="e858a47a49c4fd8cdf22b29ea7278e6b4a2bddae"; \
+	[ "$$REPLY" == "9" ] && DIFF="5" && REVISION="7272840d7db98a88f5c8b2882cc78d7ddc04e5e6"; \
 	echo "Revision       : "$$REVISION; \
 	echo ""; \
 	if [ "$$REPLY" != "1" ]; then \
