2fb4441d91bc0bda02ee427484fa9e28736ac2c3
max10 <max_10@gmx.de>
2014-06-18 15:51:13 +0200

    [NEUTRINO] rename release ordner

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -386,12 +386,12 @@ neutrino-mp-distclean:
 	rm -f $(D)/neutrino-mp.do_prepare
 
 neutrino-mp-updateyaud: neutrino-mp-clean neutrino-mp
-	mkdir -p $(prefix)/release_neutrino/usr/local/bin
-	cp $(targetprefix)/usr/local/bin/neutrino $(prefix)/release_neutrino/usr/local/bin/
-	cp $(targetprefix)/usr/local/bin/pzapit $(prefix)/release_neutrino/usr/local/bin/
-	cp $(targetprefix)/usr/local/bin/sectionsdcontrol $(prefix)/release_neutrino/usr/local/bin/
-	mkdir -p $(prefix)/release_neutrino/usr/local/sbin
-	cp $(targetprefix)/usr/local/sbin/udpstreampes $(prefix)/release_neutrino/usr/local/sbin/
+	mkdir -p $(prefix)/release/usr/local/bin
+	cp $(targetprefix)/usr/local/bin/neutrino $(prefix)/release/usr/local/bin/
+	cp $(targetprefix)/usr/local/bin/pzapit $(prefix)/release/usr/local/bin/
+	cp $(targetprefix)/usr/local/bin/sectionsdcontrol $(prefix)/release/usr/local/bin/
+	mkdir -p $(prefix)/release/usr/local/sbin
+	cp $(targetprefix)/usr/local/sbin/udpstreampes $(prefix)/release/usr/local/sbin/
 
 ################################################################################
 #
