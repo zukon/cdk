1c1625fd7c0742aa4f7f61e69f5de68536d7f667
max10 <max_10@gmx.de>
2014-08-30 23:54:42 +0200

    Revert "update delete unnecessary plugins and files e2"
    This reverts commit a58473a6494d147bbf6bcd12dd62c0ff608b56f5.
    
    Conflicts:
    	make/enigma2-release.mk

--- make/enigma2-release.mk
+++ make/enigma2-release.mk
@@ -949,52 +949,23 @@ endif
 
 	$(INSTALL_DIR) $(prefix)/release$(PYTHON_DIR)
 	cp -a $(targetprefix)$(PYTHON_DIR)/* $(prefix)/release$(PYTHON_DIR)/
-
-	rm -rf $(prefix)/release$(PYTHON_DIR)/{bsddb,compiler,curses,distutils,lib-old,lib-tk,plat-linux3,test}
-	rm -rf $(prefix)/release$(PYTHON_DIR)/ctypes/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/email/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/json/tests
-	rm -rf $(prefix)/release$(PYTHON_DIR)/lib2to3/tests
-	rm -rf $(prefix)/release$(PYTHON_DIR)/sqlite3/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/unittest/test
-
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/{test,conch,mail,names,news,words,flow,lore,pair,runner}
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/Cheetah-2.4.4-py$(PYTHON_VERSION).egg-info
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/Cheetah/Tests
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/elementtree-1.2.6_20050316-py$(PYTHON_VERSION).egg-info
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/lxml
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/lxml-2.2.8-py$(PYTHON_VERSION).egg-info
 	rm -f $(prefix)/release$(PYTHON_DIR)/site-packages/libxml2mod.so
 	rm -f $(prefix)/release$(PYTHON_DIR)/site-packages/libxsltmod.so
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/OpenSSL/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/PIL/PIL-1.1.7-py$(PYTHON_VERSION).egg-info
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/pyOpenSSL-0.11-py$(PYTHON_VERSION).egg-info
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/python_wifi-0.5.0-py$(PYTHON_VERSION).egg-info
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/pycrypto-2.5-py$(PYTHON_VERSION).egg-info
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/pyusb-1.0.0a3-py$(PYTHON_VERSION).egg-info
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/setuptools
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/setuptools-0.6c11-py$(PYTHON_VERSION).egg-info
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/zope/interface/tests
 	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/zope.interface-4.0.1-py$(PYTHON_VERSION).egg-info
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/Twisted-14.0.0-py$(PYTHON_VERSION).egg-info
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/application/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/conch/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/internet/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/lore/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/mail/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/manhole/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/names/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/news/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/pair/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/persisted/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/protocols/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/python/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/runner/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/scripts/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/trial/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/web/test
-	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/words/test
+	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/Twisted-13.0.0-py$(PYTHON_VERSION).egg-info
+	rm -rf $(prefix)/release$(PYTHON_DIR)/site-packages/twisted/{test,conch,mail,manhole,names,news,trial,words,application,enterprise,flow,lore,pair,runner,scripts,tap,topfiles}
+	rm -rf $(prefix)/release$(PYTHON_DIR)/{bsddb,compiler,curses,distutils,lib-old,lib-tk,plat-linux3,test}
 
 #
 # Dont remove pyo files, remove pyc instead
