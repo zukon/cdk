92e8963086f7174d1cd45159f9ecd4dc0390bb67
max10 <max_10@gmx.de>
2014-12-20 11:17:27 +0100

    version bump GCC_VER 4.8.3-138

--- make/bootstrap.mk
+++ make/bootstrap.mk
@@ -25,7 +25,7 @@ GLIBC_VER     = 2.14.1-50
 else
 # 4.8.3
 BINUTILS_VER  = 2.24.51.0.3-75
-GCC_VER       = 4.8.3-137
+GCC_VER       = 4.8.3-138
 LIBGCC_VER    = 4.8.3-147
 GLIBC_VER     = 2.14.1-51
 endif
