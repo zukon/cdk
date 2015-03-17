2b05a025da1a7b6eae2d58f0e902f5c73b6c162e
max10 <max_10@gmx.de>
2015-03-17 14:10:32 +0100

    update neutrino-plugins.mk

--- make/neutrino-plugins.mk
+++ make/neutrino-plugins.mk
@@ -67,11 +67,12 @@ NEUTRINO_HD2_PLUGINS_PATCHES =
 $(D)/nhd2-plugins.do_prepare:
 	rm -rf $(sourcedir)/nhd2-plugins
 	rm -rf $(sourcedir)/nhd2-plugins.org
-	[ -d "$(archivedir)/nhd2-plugins.svn" ] && \
-	(cd $(archivedir)/nhd2-plugins.svn; svn up ; cd "$(buildprefix)";); \
-	[ -d "$(archivedir)/nhd2-plugins.svn" ] || \
-	svn co http://neutrinohd2.googlecode.com/svn/branches/plugins $(archivedir)/nhd2-plugins.svn; \
-	cp -ra $(archivedir)/nhd2-plugins.svn $(sourcedir)/nhd2-plugins; \
+	[ -d "$(archivedir)/nhd2-plugins.git" ] && \
+	(cd $(archivedir)/nhd2-plugins.git; svn up ; cd "$(buildprefix)";); \
+	[ -d "$(archivedir)/nhd2-plugins.git" ] || \
+	svn co http://neutrinohd2.googlecode.com/svn/branches/plugins $(archivedir)/nhd2-plugins.git; \
+	cp -ra $(archivedir)/nhd2-plugins.git $(sourcedir)/nhd2-plugins; \
+	(cd $(sourcedir)/nhd2-plugins; git checkout plugins; cd "$(buildprefix)";); \
 	cp -ra $(sourcedir)/nhd2-plugins $(sourcedir)/nhd2-plugins.org
 	for i in $(NEUTRINO_HD2_PLUGINS_PATCHES); do \
 		echo "==> Applying Patch: $(subst $(PATCHES)/,'',$$i)"; \
