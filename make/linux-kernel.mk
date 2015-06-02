a3d80c9d4bf75f90a4dbd6f0b165ce123295fdc0
max10 <max_10@gmx.de>
2015-06-02 02:12:25 +0200

    add linux-perf-warning-fix_stm24_0217.patch

--- make/linux-kernel.mk
+++ make/linux-kernel.mk
@@ -23,6 +23,7 @@ COMMONPATCHES_24 = \
 		linux-tune_stm24.patch \
 		linux-sh4-permit_gcc_command_line_sections_stm24.patch \
 		linux-sh4-mmap_stm24.patch \
+		$(if $(P0217),linux-perf-warning-fix_stm24$(PATCH_STR).patch) \
 		$(if $(P0215)$(P0217),linux-ratelimit-bug_stm24$(PATCH_STR).patch) \
 		$(if $(P0215)$(P0217),linux-patch_swap_notify_core_support_stm24$(PATCH_STR).patch) \
 		$(if $(P0209),linux-sh4-dwmac_stm24_0209.patch) \
