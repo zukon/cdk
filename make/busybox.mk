d0f2a3581e53429c2d72e3903fdb9635fe7f988d
max10 <max_10@gmx.de>
2015-11-28 18:52:09 +0100

    Revert "[busybox] add busybox-1.24.1-unzip.patch"
    This reverts commit 0e0d396da334de6e585bac67a38fae5428cece07.

--- make/busybox.mk
+++ make/busybox.mk
@@ -4,7 +4,6 @@
 $(D)/busybox: $(D)/bootstrap @DEPENDS_busybox@ $(buildprefix)/Patches/busybox.config$(if $(UFS912)$(UFS913)$(SPARK)$(SPARK7162),_nandwrite)
 	@PREPARE_busybox@
 	cd @DIR_busybox@ && \
-		patch -p1 < $(PATCHES)/busybox-1.24.1-unzip.patch && \
 		patch -p1 < $(PATCHES)/busybox-1.24.1-ifupdown.patch && \
 		patch -p1 < $(PATCHES)/busybox-1.24.1-unicode.patch && \
 		patch -p1 < $(PATCHES)/busybox-1.24.1-extra.patch && \
