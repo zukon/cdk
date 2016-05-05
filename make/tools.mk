05f4e43feb061cdf45476a5f02d87b7a8557029f
max10 <max_10@gmx.de>
2016-05-06 00:13:54 +0200

    add tools DRIVER_TOPDIR

--- make/tools.mk
+++ make/tools.mk
@@ -14,7 +14,7 @@ $(appsdir)/tools/config.status: bootstrap driver bzip2 libpng libjpeg ffmpeg
 	$(if $(EPLAYER3), --enable-eplayer3)
 
 $(D)/tools: $(appsdir)/tools/config.status
-	$(MAKE) -C $(appsdir)/tools all prefix=$(targetprefix) \
+	$(MAKE) -C $(appsdir)/tools all prefix=$(targetprefix) DRIVER_TOPDIR=$(driverdir) \
 	CPPFLAGS="\
 	-I$(targetprefix)/usr/include \
 	-I$(driverdir)/bpamem \
@@ -23,6 +23,6 @@ $(D)/tools: $(appsdir)/tools/config.status
 	-I$(driverdir)/include/player2 \
 	$(if $(PLAYER191), -DPLAYER191) \
 	" && \
-	$(MAKE) -C $(appsdir)/tools install prefix=$(targetprefix)
+	$(MAKE) -C $(appsdir)/tools install prefix=$(targetprefix) DRIVER_TOPDIR=$(driverdir)
 	touch $@
 
